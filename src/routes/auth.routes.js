7ca72162a331639e7a948519c5ba0273a44bed4a
@RupertoTacuche <murcio2490@gmail.com>
2024-10-02 12:33:05 -0600

    backend_terminado

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,12 +6,14 @@ import {
         profile} from '../controlllers/auth.controller.js'
         
 import {authRequired} from "../middlewares/validateToken.js"
+import { validateSchema} from '../middlewares/validator.middleware.js'
+import { registerSchema, loginSchema} from '../schemas/auth.schema.js'
 
 const router = Router()
 
 /* exporto las rutas */
-router.post('/register', register)
-router.post('/login', login)
+router.post('/register', validateSchema(registerSchema) , register)
+router.post('/login', validateSchema(loginSchema) , login)
 router.post("/logout", logout)
 
 router.get("/profile", authRequired, profile)
