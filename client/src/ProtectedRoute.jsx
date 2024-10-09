fbc42e35c81d24f8e3bbc74e445be961991194f5
@RupertoTacuche <murcio2490@gmail.com>
2024-10-09 00:07:51 -0600

    tasks_p1

--- client/src/ProtectedRoute.jsx
+++ client/src/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import {Navigate, Outlet} from 'react-router-dom'
 import {useAuth} from './context/AuthContext'
 function ProtectedRoute() {
     const {loading, isAuthenticated} = useAuth()
-    console.log(loading, isAuthenticated)
+    //console.log(loading, isAuthenticated)
 
     if(loading) return <h1>
       Loading...
