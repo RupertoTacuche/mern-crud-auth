91638bb0246f84e2382b607ac14c7e0bf1818271
@RupertoTacuche <murcio2490@gmail.com>
2024-10-10 02:14:48 -0600

    task_p2

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -2,6 +2,8 @@ import {createContext, useState, useContext, useEffect} from 'react'
 import {registerRequest, loginRequest, verifyTokenRequest}  from '../api/auth'
 import Cookies from 'js-cookie'
 
+
+
 export const AuthContext = createContext()
 
 export const useAuth = () => {
