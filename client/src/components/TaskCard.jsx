9d4d8b6fc42362a197bce17eb738cebf6dc46767
@RupertoTacuche <murcio2490@gmail.com>
2024-10-13 21:03:36 -0600

    task_delete_final

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,16 +1,22 @@
-
+import {useTasks} from "../context/TasksContext"
+import {Link} from 'react-router-dom'
 
 function TaskCard({task}) {
     //console.log(task)
+    const {deleteTask}= useTasks()
+    
   return (
     <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
       <header className="flex justify-between">
         <h1 className="text-2xl font-bold">{task.title}</h1>
         <div className="flex gap-x-2 items-center">
           <button onClick= {() => {
-            console.log(task._id)
-          }}>delete</button>
-          <button>edit</button>
+            deleteTask(task._id)
+          }}
+          >
+            delete
+            </button>
+            <Link to={`/tasks/${task._id}`}>Edit</Link>
         </div>
       </header>
       <p className="text-slate-300">{task.description}</p>
